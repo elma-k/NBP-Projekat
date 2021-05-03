8949e58b9a922459cbf0343fcdb33615190d9636
afajic1 <afajic1@etf.unsa.ba>
2021-05-04 01:19:38 +0200

    add gateway and eureka settings

--- musica/src/main/java/com/example/musica/MusicaApplication.java
+++ musica/src/main/java/com/example/musica/MusicaApplication.java
@@ -2,8 +2,12 @@ package com.example.musica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.*;
+import org.springframework.cloud.openfeign.*;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MusicaApplication {
 
     public static void main(String[] args) {
--- musica/src/main/java/com/example/musica/service/AlbumService.java
+++ musica/src/main/java/com/example/musica/service/AlbumService.java
@@ -19,12 +19,12 @@ public class AlbumService {
     RequestValidation requestValidation;
 
     @Autowired
-    AlbumRepository AlbumRepository;
+    AlbumRepository albumRepository;
 
 
     @GetMapping("/all")
     public List<Album> findAll(){
-        List<AlbumEntity> Albums=AlbumRepository.findAll();
+        List<AlbumEntity> Albums=albumRepository.findAll();
         List<Album> responses=new ArrayList<Album>();
         for(AlbumEntity albumEntity :Albums) {
             Album Album = new Album();
@@ -39,13 +39,13 @@ public class AlbumService {
 
         AlbumEntity sobaEntity = new AlbumEntity();
         BeanUtils.copyProperties(AlbumRequest, sobaEntity);
-        AlbumRepository.save(sobaEntity);
+        albumRepository.save(sobaEntity);
         return "Successfully created!";
     }
 
     @PutMapping("/{id}")
    public String update(@PathVariable Long id, @RequestBody AlbumRequest Album) {
-        Optional<AlbumEntity> AlbumEntity = AlbumRepository.findById(id);
+        Optional<AlbumEntity> AlbumEntity = albumRepository.findById(id);
 
         if (AlbumEntity == null) {
             return "Album with id"+id+" does not exist!";
@@ -53,7 +53,7 @@ public class AlbumService {
             AlbumEntity AlbumEntity1=AlbumEntity.get();
 
             BeanUtils.copyProperties(Album ,AlbumEntity1);
-            AlbumRepository.save(AlbumEntity1);
+            albumRepository.save(AlbumEntity1);
             return "Updated successfully!";
         }
 
@@ -62,12 +62,12 @@ public class AlbumService {
    public String deleteAlbum(@PathVariable Long id) {
          if(requestValidation.validateAlbumId(id) != null)
          return requestValidation.validateAlbumId(id);
-        AlbumRepository.deleteById(id);
+        albumRepository.deleteById(id);
         return "Album is deleted successfully";
     }
     @GetMapping("/{id}")
    public Album findAlbumById(@PathVariable Long id) {
-        AlbumEntity AlbumEntity= AlbumRepository.findById(id).get();
+        AlbumEntity AlbumEntity= albumRepository.findById(id).get();
         Album Album=new Album();
         BeanUtils.copyProperties(AlbumEntity,Album);
         return Album;
