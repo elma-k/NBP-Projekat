65e600394ee02e0280bbbf6bb9d912df80a1af29
ekupusovic1 <ekupusovic1@etf.unsa.ba>
2021-05-08 17:46:48 +0200

    preview page added, overall fixes

--- musica-frontend/src/app/app.module.ts
+++ musica-frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PreviewpageComponent } from './previewpage/previewpage.component';
+import { RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewpageComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: 'previewpage', component: PreviewpageComponent},
+      {path: 'landing-page', component: LandingPageComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ musica-frontend/src/app/previewpage/previewpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-previewpage',
+  templateUrl: './previewpage.component.html',
+  styleUrls: ['./previewpage.component.css']
+})
+export class PreviewpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
