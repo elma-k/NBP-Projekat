66924169b010d52ce2d2457a7fbadf38629e43de
ekupusovic1 <ekupusovic1@etf.unsa.ba>
2021-06-03 12:49:00 +0200

    details-album fixes

--- musica-frontend/src/app/app.module.ts
+++ musica-frontend/src/app/app.module.ts
@@ -31,6 +31,40 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SecondsToMinutesPipe } from './pipe/seconds-to-minutes';
 import { DetailsSongComponent } from './details-song/details-song.component';
 import { DetailsAlbumComponent } from './details-album/details-album.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {A11yModule} from '@angular/cdk/a11y';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {PortalModule} from '@angular/cdk/portal';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
@@ -75,6 +109,52 @@ import { DetailsAlbumComponent } from './details-album/details-album.component';
   entryComponents: [ LoginFormModalComponent,
   ProfilePageComponent ],
   providers: [MDBSpinningPreloader],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    A11yModule,
+    ClipboardModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    OverlayModule,
+    PortalModule,
+    ScrollingModule,
+  ]
 })
 export class AppModule { }
--- musica-frontend/src/app/details-album/details-album.component.ts
+++ musica-frontend/src/app/details-album/details-album.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+export interface PeriodicElement {
+  title: string;
+  position: number;
+  duration: number;
+  liked: boolean;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, title: 'Song1', duration: 1.0079, liked: false },
+  {position: 2, title: 'Song2', duration: 4.0026, liked: true},
+  {position: 3, title: 'Song3', duration: 6.941, liked: false},
+  {position: 4, title: 'Song4', duration: 9.0122, liked: true},
+];
+
 
 @Component({
   selector: 'app-details-album',
@@ -7,15 +21,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsAlbumComponent implements OnInit {
 
-  constructor() { }
-
   albumPhotoSrc="./assets/images/dubioza-kolektiv.jpg";
   albumTitle= "Happy Machine";
   performer= "Dubioza Kolektiv";
   year = 2016;
   noSongs =10;
   fullLength ="35 min, 6 sec";
+  
+  constructor() {
+  }
+  displayedColumns: string[] = ['position', 'title',  'liked', 'duration'];
+  dataSource = ELEMENT_DATA;
   ngOnInit(): void {
   }
-
+  
 }
