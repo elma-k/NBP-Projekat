2643d3ce6c6e35af8df80125882c874074d5fe10
Edina <edinaosmanspahic@gmail.com>
2021-06-09 18:27:35 +0200

    login redirect

--- musica-frontend/src/app/login-form-modal/login-form-modal.component.ts
+++ musica-frontend/src/app/login-form-modal/login-form-modal.component.ts
@@ -3,6 +3,8 @@ import { MDBModalRef } from 'ng-uikit-pro-standard';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {environment} from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
 @Component({
   selector: 'app-login-form-modal',
   templateUrl: './login-form-modal.component.html',
@@ -15,7 +17,7 @@ password:string;
 private API_BASE_URL = environment.API_BASE_URL;
 
 private REST_API_SERVER = this.API_BASE_URL+"/auth/login";
-  constructor(public modalRef: MDBModalRef,private httpClient: HttpClient) {
+  constructor(public modalRef: MDBModalRef,private httpClient: HttpClient,private router: Router) {
   }
 
   ngOnInit() {
@@ -31,13 +33,14 @@ private REST_API_SERVER = this.API_BASE_URL+"/auth/login";
   get loginFormModalPassword() {
     return this.validatingForm.get('loginFormModalPassword');
   }
- 
+
   onSave(){
     console.log(this.email);
     console.log(this.password);
     this.httpClient.post(this.REST_API_SERVER,{email:this.email, password:this.password}
     ).subscribe((data: any)=>{
       console.log(data);
+      this.router.navigate(['/previewpage']);
      // this.registerForm.reset();
       });
   }
